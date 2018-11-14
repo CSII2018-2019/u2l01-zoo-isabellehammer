f25a9ffbf06e37dad5f8c392f686a17f730e35f9
Isabelle <hammeri@theellisschool.org>
2018-11-13 21:31:08 -0500

    what I have so far

---
+++ src/U2L01animal.java
@@ -0,0 +1,9 @@
+
+public class U2L01animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/U2L01zoo.java
@@ -0,0 +1,9 @@
+
+public class U2L01zoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

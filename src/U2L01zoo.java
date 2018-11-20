c10b8e9420d109a779c6b886e4ca044d4370cd10
Isabelle <hammeri@theellisschool.org>
2018-11-19 21:24:32 -0500

    ?

--- src/U2L01zoo.java
+++ src/U2L01zoo.java
@@ -1,8 +1,18 @@
+import java.awt.Color;
 
 public class U2L01zoo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//create using default constructor
+		U2L01animal a1 = new U2L01animal();
+		a1.setSkinFurFeathersEtc(Color.RED); //call a method on an instance of an object
+		a1.setWeightLbs(100);
+		System.out.println(a1);//print calls toString method
+		
+		//create using overloaded constructor
+		U2L01animal a2 = new U2L01animal(5, 120, Color.YELLOW, 4);
+		
+		
 
 	}
 
